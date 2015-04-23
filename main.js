b8d3b56fb63a146e1b165cfee305381d78ee0075
Ben Haaland <bphaaland@gmail.com>
2015-04-23 17:08:33 -0400

    updating a lot of stuff

--- main.js
+++ main.js
@@ -7,68 +7,58 @@ var userTypes = [{
         user     : 'Current Member',
         id       : 'js-logic-path-current-member',
         content  : 'You are awesome as a current member!',
-        form     : '<a href="https://google.com">Google</a>'
+        form     : '<a href="https://google.com">Link to a form for current members</a>'
     },
     {
         user     : 'Future Member',
         id       : 'js-logic-path-future-member',
         content  : 'You are awesome for thinking about being a member!',
-        form     : 'futureMemberForm'
+        form     : '<a href="https://google.com">Link to a form for new members</a>'
     },
     {
         user     : 'Employer',
         id       : 'js-logic-path-employer',
         content  : 'You should hire all of us cause we are awesome!',
-        form     : 'employerForm'
+        form     : '<a href="https://google.com">Link to a form for employers</a>'
     },
     {
         user     : 'Other',
         id       : 'js-logic-path-other',
         content  : 'You are awesome...',
-        form     : 'otherForm'
+        form     : '<a href="https://google.com">Link to a form for other peeps</a>'
     }
 ];
 
+
+// This is the main js function that listens for onclick events. when an
+// onclick event happens, it runs contentChanger()
+
 function triggerView(){
-    for (var i = 0; i < userTypes.length; i++){
-        document.getElementById(userTypes[i].id).onclick = function(){
-            document.getElementById('js-user').innerHTML    = userTypes[0].user;
-            document.getElementById('js-content').innerHTML = userTypes[0].content;
-            document.getElementById('js-form').innerHTML    = userTypes[0].form;
-        }
+
+    // The contentChanger() function has some funky stuff going on. 'this'
+    // in this context is (userTypes[i]) because we bind it in the onclick
+    // function below. (See comment above the for loop).
+
+    // The first part of the contentChanger() function has access to the
+    // key value pairs of the objects in the userTypes array, and can
+    // insert them into the DOM.
+
+    function contentChanger(){
+        document.getElementById('js-user').innerHTML    = this.user;
+        document.getElementById('js-content').innerHTML = this.content;
+        document.getElementById('js-form').innerHTML    = this.form;
+    }
+
+    // Here we are looping through our array of objects, userTypes. When
+    // any of these elements are clicked, we bind that object to the
+    // contentChanger() function. This way it can access its key value
+    // pairs when we want to insert the HTML into the DOM.
+
+    for (var i = 0; i < userTypes.length; i++) {
+        document.getElementById(userTypes[i].id).onclick = contentChanger.bind(userTypes[i]);
     }
 }
 
+// start listening for that sheezy
+
 triggerView();
-
-//// This function targets the element that was clicked by the user and inserts
-//// the relevant information into the DOM for that specific user.
-//
-//function triggerLogicPath() {
-//
-//    // Old Member path
-//    userTypes.oldMember.onclick = function() {
-//        console.log(this);
-//    };
-//
-//    // New Member path
-//    userTypes.newMember.onclick = function() {
-//        console.log(this);
-//    };
-//
-//    // Employer path
-//    userTypes.employerMember.onclick = function() {
-//        console.log(this);
-//    };
-//
-//    // Other path
-//    userTypes.otherMember.onclick = function() {
-//        console.log(this);
-//    };
-//}
-//
-//triggerLogicPath();
-//
-////console.log( Object.keys(typesOfUsers) );
-
-
