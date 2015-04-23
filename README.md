92f422dcd180465c5c20a58f8a3c5a6895473c0b
Ben Haaland <bphaaland@gmail.com>
2015-04-23 15:42:31 -0400

    continuing on the path to destruction

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 <h1>JavaScript Logic Path</h1>
-<p>This project was made for our JSGuild DC web page. To see it in action (when it's done) Check it out here : <a href="http://jsguild-dc.github.io/">JSGuild-DC</a></p>
+<p>This project was made for our JSGuild DC web page. To see it in action (when it's done) check it out here : <a href="http://jsguild-dc.github.io/">JSGuild-DC</a></p>
 
 <h2>Purpose:</h2>
   <ol>
--- main.js
+++ main.js
@@ -1,43 +1,74 @@
 
-// Creates an object with all of the types of users that can visit our site.
+// Creates an array full of objects, or users, that can visit our site.
 // This makes it easier to add or remove types of visitors without having
 // to change them in multiple places.
 
-var typesOfUsers = {
-    oldMember      : document.getElementById('js-logicPathOldMember'),
-    newMember      : document.getElementById('js-logicPathNewMember'),
-    employerMember : document.getElementById('js-logicPathEmployer'),
-    otherMember    : document.getElementById('js-logicPathOther')
-};
+var userTypes = [{
+        user     : 'Current Member',
+        id       : 'js-logic-path-current-member',
+        content  : 'You are awesome as a current member!',
+        form     : '<a href="https://google.com">Google</a>'
+    },
+    {
+        user     : 'Future Member',
+        id       : 'js-logic-path-future-member',
+        content  : 'You are awesome for thinking about being a member!',
+        form     : 'futureMemberForm'
+    },
+    {
+        user     : 'Employer',
+        id       : 'js-logic-path-employer',
+        content  : 'You should hire all of us cause we are awesome!',
+        form     : 'employerForm'
+    },
+    {
+        user     : 'Other',
+        id       : 'js-logic-path-other',
+        content  : 'You are awesome...',
+        form     : 'otherForm'
+    }
+];
 
-// This function targets the element that was clicked by the user and inserts
-// the relevant information into the DOM for that specific user.
-
-function triggerLogicPath() {
-
-    // Old Member path
-    typesOfUsers.oldMember.onclick = function() {
-        console.log(this);
-    };
-
-    // New Member path
-    typesOfUsers.newMember.onclick = function() {
-        console.log(this);
-    };
-
-    // Employer path
-    typesOfUsers.employerMember.onclick = function() {
-        console.log(this);
-    };
-
-    // Other path
-    typesOfUsers.otherMember.onclick = function() {
-        console.log(this);
-    };
+function triggerView(){
+    for (var i = 0; i < userTypes.length; i++){
+        document.getElementById(userTypes[i].id).onclick = function(){
+            document.getElementById('js-user').innerHTML    = userTypes[0].user;
+            document.getElementById('js-content').innerHTML = userTypes[0].content;
+            document.getElementById('js-form').innerHTML    = userTypes[0].form;
+        }
+    }
 }
 
-triggerLogicPath();
+triggerView();
 
-//console.log( Object.keys(typesOfUsers) );
+//// This function targets the element that was clicked by the user and inserts
+//// the relevant information into the DOM for that specific user.
+//
+//function triggerLogicPath() {
+//
+//    // Old Member path
+//    userTypes.oldMember.onclick = function() {
+//        console.log(this);
+//    };
+//
+//    // New Member path
+//    userTypes.newMember.onclick = function() {
+//        console.log(this);
+//    };
+//
+//    // Employer path
+//    userTypes.employerMember.onclick = function() {
+//        console.log(this);
+//    };
+//
+//    // Other path
+//    userTypes.otherMember.onclick = function() {
+//        console.log(this);
+//    };
+//}
+//
+//triggerLogicPath();
+//
+////console.log( Object.keys(typesOfUsers) );
 
 
